5a890607549d3f176ec386502cc72db08a3f4585
PoornachanderReddy <poornachander295@gmail.com>
2025-05-18 11:13:42 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { Link, Route, Switch } from "react-router-dom";
 
 const App = () => {
+  const About = () => (
+    <>
+      <h1>About</h1>
+      <p>This is a sample react router project</p>
+    </>
+  );
+  const Home = () => <h1>Welcome to my website!</h1>;
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
-export default App
+export default App;
